576633ceb2fd752de18823162405b8ee417c8cf8
Ibro <ibro@ibro.me>
2017-04-09 11:57:13 +0200

    yield! resume! yield!

--- main.ts
+++ main.ts
@@ -30,19 +30,43 @@
 // }
 
 
-function print(a) {
-    console.log('a: ' + a); // prints out 192
+// function print(a) {
+//     console.log('a: ' + a); // prints out 192
+// }
+
+// function* generator() { 
+//     print(yield 'Iterator result value'); // wait so we can pass parameter to print function
+// }
+
+// var iterator = generator();
+
+// // when calling .next() for the first time we never pass value since it will get ignored in any case
+// let iteratorResult = iterator.next(); 
+// console.log(iteratorResult); // Object {value: "Iterator result value", done: false}
+
+// iteratorResult = iterator.next(192); // this will be passed as parameter to function print() inside of generator() function
+// console.log(iteratorResult); // Object {value: undefined, done: true}
+
+function* generator(a) {
+    var b = 5 * (yield (a + 5));
+    console.log('b:', b); // 40
+
+    var c = yield (b / 10);
+    console.log('c:', c); // 4
+
+    let total = a + b + c;
+    yield total / 2;
+
+    console.log('a + b + c:', total); // 50
 }
 
-function* generator() { 
-    print(yield 'Iterator result value'); // wait so we can pass parameter to print function
-}
+var it = generator(6);
 
-var iterator = generator();
+// we do not send anything into next() because it gets ignored during first time
+console.log(it.next());   // {value: 11, done: false}
 
-// when calling .next() for the first time we never pass value since it will get ignored in any case
-let iteratorResult = iterator.next(); 
-console.log(iteratorResult); // Object {value: "Iterator result value", done: false}
+console.log(it.next(8)); // {value: 4, done: false}
 
-iteratorResult = iterator.next(192); // this will be passed as parameter to function print() inside of generator() function
-console.log(iteratorResult); // Object {value: undefined, done: true}
+console.log(it.next(4)); // {value: 25, done: false}
+
+console.log(it.next(10)); // {value: undefined, done: true}
